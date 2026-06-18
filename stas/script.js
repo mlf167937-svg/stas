cd65089f25726cbb30281da25a2a91e2374cafeb
mlf167937-svg <mlf167937@gmail.com>
2026-06-18 17:18:31 +0700

    Update script.js

--- stas/script.js
+++ stas/script.js
@@ -1,1 +1,7 @@
+function showPage(page) {
 
+  let pages = document.querySelectorAll(".page");
+  pages.forEach(p => p.classList.remove("active"));
+
+  document.getElementById(page).classList.add("active");
+}
